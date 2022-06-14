48bc4560be9563144c08ec5f275230d05e31108e
dejwi <misti.krasula@gmail.com>
2022-06-14 13:12:08 +0200

    new theme settings

--- theme/index.ts
+++ theme/index.ts
@@ -7,4 +7,12 @@ export const NavColors = {
 export const MainColors = {
   bgPrimary: "#F5F7FB",
   bgSecondary: "#ffffff",
+  primary: "#09113C",
+  secondary: "#666F9E",
+};
+
+export const TimetableColors = {
+  selected: "#6C54FE",
+  arrowRight: "#7673FF",
+  arrowBack: "#7F46F8",
 };
