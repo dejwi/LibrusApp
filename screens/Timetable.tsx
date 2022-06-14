8eae494812287c64383b8db696e187404e8f7f63
dejwi <misti.krasula@gmail.com>
2022-06-14 17:27:45 +0200

    added lessons & fixed style issue

--- screens/Timetable.tsx
+++ screens/Timetable.tsx
@@ -6,6 +6,7 @@ import Nav from "../components/Nav";
 import Top from "../components/Timetable/Top/Top";
 import { MainColors } from "../theme";
 import moment from "moment";
+import Lessons from "../components/Timetable/Lessons/Lessons";
 
 const Timetable = () => {
   // week days in month
@@ -13,16 +14,19 @@ const Timetable = () => {
   // 1-5 week day
   const [selected, setSelected] = useState(moment().day());
 
-  if (!currentWeek) return null;
   return (
     <View className="flex-1" style={{ backgroundColor: MainColors.bgPrimary }}>
-      <SeparatedStatusBar />
-      <Top
-        days={currentWeek}
-        selected={selected}
-        setSelected={(e) => setSelected(e)}
-      />
-
+      {!currentWeek ? null : (
+        <>
+          <SeparatedStatusBar />
+          <Top
+            days={currentWeek}
+            selected={selected}
+            setSelected={(e) => setSelected(e)}
+          />
+          <Lessons selected={selected} />
+        </>
+      )}
       <Nav current="Timetable" />
     </View>
   );
