ed23170cef437d5c87ca608ea296789e378fe292
dejwi <misti.krasula@gmail.com>
2022-06-21 18:13:17 +0200

    added hiding nav

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { TailwindProvider } from "tailwindcss-react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  NavigationState,
+} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import "tailwindcss-react-native/types.d";
 import { useEffect, useState } from "react";
+
+import { useNavigation } from "@react-navigation/native";
 
 import Home from "./screens/Home";
 import Timetable from "./screens/Timetable";
@@ -22,18 +28,24 @@ const theme = {
 };
 
 export default function App() {
+  const [showNav, setShowNav] = useState(false);
   const [loaded] = useFonts({
     PoppinsSemiBold: require("./assets/fonts/Poppins-SemiBold.ttf"),
     PoppinsMedium: require("./assets/fonts/Poppins-Medium.ttf"),
     PoppinsRegular: require("./assets/fonts/Poppins-Regular.ttf"),
     PoppinsLight: require("./assets/fonts/Poppins-Light.ttf"),
   });
+  const printState = (state: NavigationState | undefined) => {
+    if (!state) return;
+    const rname = state.routes[state.index].name;
+    if (rname === "Login" || rname === "Loading") setShowNav(false);
+    else setShowNav(true);
+  };
 
   if (!loaded) return null;
-
   return (
     <TailwindProvider>
-      <NavigationContainer theme={theme}>
+      <NavigationContainer theme={theme} onStateChange={printState}>
         <Stack.Navigator
           screenOptions={{ headerShown: false, gestureEnabled: false }}
           initialRouteName="Loading"
@@ -49,7 +61,7 @@ export default function App() {
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Grades" component={Grades} />
         </Stack.Navigator>
-        <Nav />
+        <Nav show={showNav} />
       </NavigationContainer>
     </TailwindProvider>
   );
